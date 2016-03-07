59375ff427fee90d9afa17ed7604d7f02b82bba1
Chris Jones <chrisjo@zillow.com>
2016-03-07 13:23:29 -0800

    html boilerplate for theme index

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title><?php wp_title(); ?></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+  <?php wp_head(); ?>
+</head>
+<body>
+
+  <h1>WPBP</h1>
+  <p>A WordPress Boilerplate Theme</p>
+
+  <?php wp_footer(); ?>
+</body>
+</html>
