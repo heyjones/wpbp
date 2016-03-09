f3bb528b85fada4fce16b42bf59cafa838145fdb
Chris Jones <chrisjo@zillow.com>
2016-03-08 16:05:28 -0800

    added loop to index

--- index.php
+++ index.php
@@ -13,6 +13,23 @@
   <h1>WPBP</h1>
   <p>A WordPress Boilerplate Theme</p>
 
+  <?php
+  if ( have_posts() ) {
+    while ( have_posts() ) {
+      the_post();
+      ?>
+
+      <h2><?php the_title(); ?></h2>
+      <?php the_content(); ?>
+      <a href="<?php the_permalink(); ?>">Permalink</a>
+
+      <?php
+    }
+  }
+  ?>
+
+  <?php the_posts_pagination(); ?>
+
   <?php wp_footer(); ?>
 </body>
 </html>
