36f8fe65937f84d8c777e4498078e03762d542c1
Chris Jones <chrisjo@zillow.com>
2016-03-08 16:39:37 -0800

    fix for stylesheet enqueue

--- functions.php
+++ functions.php
@@ -1,12 +1,8 @@
 <?php
 
-function wpbp_enqueue_style(){
-  wp_enqueue_style( 'wpbp', get_template_directory() . '/dist/styles/wpbp.css' );
-}
-add_action( 'wp_enqueue_style', 'wpbp_enqueue_style' );
-
 function wpbp_enqueue_scripts(){
   wp_deregister_script( 'jquery' );
-  wp_enqueue_script( 'wpbp', get_template_directory() . '/dist/scripts/wpbp.js', NULL, NULL, true );
+  wp_enqueue_style( 'wpbp', get_template_directory_uri() . '/dist/styles/wpbp.css' );
+  wp_enqueue_script( 'wpbp', get_template_directory_uri() . '/dist/scripts/wpbp.js', NULL, NULL, true );
 }
 add_action( 'wp_enqueue_scripts', 'wpbp_enqueue_scripts' );
