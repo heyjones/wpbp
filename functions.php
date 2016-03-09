b58f819472bcb2f01aeb64c6587c5bf2e16cfd3e
Chris Jones <chrisjo@zillow.com>
2016-03-08 16:22:39 -0800

    removed jquery dependency from wpbp.js

--- functions.php
+++ functions.php
@@ -7,6 +7,6 @@ add_action( 'wp_enqueue_style', 'wpbp_enqueue_style' );
 
 function wpbp_enqueue_scripts(){
   wp_deregister_script( 'jquery' );
-  wp_enqueue_script( 'wpbp', get_template_directory() . '/dist/scripts/wpbp.js', array( 'jquery' ), NULL, true );
+  wp_enqueue_script( 'wpbp', get_template_directory() . '/dist/scripts/wpbp.js', NULL, NULL, true );
 }
 add_action( 'wp_enqueue_scripts', 'wpbp_enqueue_scripts' );
