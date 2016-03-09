856561238895eb52c6eeea0f30a4fcfc6a2f75b4
Chris Jones <chrisjo@zillow.com>
2016-03-08 16:14:44 -0800

    fix for enqueue functions

--- functions.php
+++ functions.php
@@ -1,12 +1,12 @@
 <?php
 
-function wp_enqueue_style(){
+function wpbp_enqueue_style(){
   wp_enqueue_style( 'wpbp', get_template_directory() . '/dist/styles/wpbp.css' );
 }
-add_action( 'wp_enqueue_style', array( $this, wpbp_enqueue_style ) );
+add_action( 'wp_enqueue_style', 'wpbp_enqueue_style' );
 
 function wpbp_enqueue_scripts(){
   wp_deregister_script( 'jquery' );
   wp_enqueue_script( 'wpbp', get_template_directory() . '/dist/scripts/wpbp.js', array( 'jquery' ), NULL, true );
 }
-add_action( 'wp_enqueue_scripts', array( $this, wpbp_enqueue_scripts ) )
+add_action( 'wp_enqueue_scripts', 'wpbp_enqueue_scripts' );
