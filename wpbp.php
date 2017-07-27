a723ef02e9dd8750de7c742a13f733b776d805d5
Chris Jones <chris@>
2017-07-27 11:51:37 -0700

    replace wpbp class with namespace

--- wpbp.php
+++ wpbp.php
@@ -1,23 +1,17 @@
 <?php
 
-if( !class_exists( 'wpbp' ) ){
+namespace wpbp;
 
-  class wpbp{
+add_action( 'init', __NAMESPACE__ . '\\init' );
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\wp_enqueue_scripts' );
 
-    public function __construct(){
-      add_action( 'wp_enqueue_scripts', array( $this, 'wpbp_enqueue_scripts' ) );
-      add_theme_support( 'title-tag' );
-    }
+function init(){
+  add_theme_support( 'title-tag' );
+}
 
-    function wpbp_enqueue_scripts(){
-      wp_deregister_script( 'jquery' );
-      if( file_exists( get_template_directory() . '/dist/styles/wpbp.css' ) ) wp_enqueue_style( 'wpbp-styles', get_template_directory_uri() . '/dist/styles/wpbp.css', NULL, filemtime( get_template_directory() . '/dist/styles/wpbp.css' ) );
-      if( file_exists( get_template_directory() . '/dist/icons/wpbp.css' ) ) wp_enqueue_style( 'wpbp-icons', get_template_directory_uri() . '/dist/icons/wpbp.css', NULL, filemtime( get_template_directory() . '/dist/icons/wpbp.css' ) );
-      if( file_exists( get_template_directory() . '/dist/scripts/wpbp.js' ) ) wp_enqueue_script( 'wpbp-scripts', get_template_directory_uri() . '/dist/scripts/wpbp.js', NULL, filemtime( get_template_directory() . '/dist/scripts/wpbp.js' ), true );
-    }
-
-  }
-
-  new wpbp();
-
+function wp_enqueue_scripts(){
+  wp_deregister_script( 'jquery' );
+  if( file_exists( get_template_directory() . '/dist/styles/wpbp.css' ) ) wp_enqueue_style( 'wpbp-styles', get_template_directory_uri() . '/dist/styles/wpbp.css', NULL, filemtime( get_template_directory() . '/dist/styles/wpbp.css' ) );
+  if( file_exists( get_template_directory() . '/dist/icons/wpbp.css' ) ) wp_enqueue_style( 'wpbp-icons', get_template_directory_uri() . '/dist/icons/wpbp.css', NULL, filemtime( get_template_directory() . '/dist/icons/wpbp.css' ) );
+  if( file_exists( get_template_directory() . '/dist/scripts/wpbp.js' ) ) wp_enqueue_script( 'wpbp-scripts', get_template_directory_uri() . '/dist/scripts/wpbp.js', NULL, filemtime( get_template_directory() . '/dist/scripts/wpbp.js' ), true );
 }
